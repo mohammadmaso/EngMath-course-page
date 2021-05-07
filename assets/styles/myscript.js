f46eda561599317a3983a5465308467e31c33f73
Sgoudarzi79@gmail.com <Sgoudarzi79@gmail.com>
2021-05-08 01:39:53 +0430

    Fixed responsive issues

--- assets/styles/myscript.js
+++ assets/styles/myscript.js
@@ -1,7 +1,18 @@
-const bars = document.getElementById('#bars')
-const menu = document.getElementById('#mobile-menu')
+const bars = document.querySelector('#bars')
+const menu = document.querySelector('#mobile-menu')
+bars.addEventListener('click', myFunc)
+window.addEventListener('resize', () => {
+    if(window.innerWidth >= 768){
+        if(!menu.classList.contains('hidden')) {
+            menu.classList.add('hidden');
+        }
+    }
+})
+
+
 
 function myFunc(){
+    console.log(window.innerWidth)
     if(menu.classList.contains('hidden')) {
         menu.classList.remove('hidden');
     } else {
