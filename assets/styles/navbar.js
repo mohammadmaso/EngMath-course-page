e3a972aefe30cf1d0a685734b2511e22859efab4
Sgoudarzi79@gmail.com <Sgoudarzi79@gmail.com>
2021-05-11 10:39:11 +0430

    Fixed navbar ui

--- assets/styles/navbar.js
+++ assets/styles/navbar.js
@@ -1,1 +1,23 @@
-console.log("Href is :"+window.location.pathname)
+const homeNav = document.getElementById('home-nav')
+const coursesNav = document.getElementById('courses-nav')
+const visualizationNav = document.getElementById('visualization-nav')
+
+var pathName = window.location.pathname
+pathName = pathName.replace("/", "")
+pathName = pathName.replace("/", "")
+if (pathName == 'Courses') {
+    coursesNav.classList.add("bg-gray-900", "text-white", "px-3", "py-2", "rounded-md", "text-sm", "font-medium")
+    homeNav.classList.add("text-gray-300", "hover:bg-gray-700", "hover:text-white", "px-3", "py-2", "rounded-md", "text-sm", "font-medium",)
+    visualizationNav.classList.add("text-gray-300", "hover:bg-gray-700", "hover:text-white", "px-3", "py-2", "rounded-md", "text-sm", "font-medium",)
+}
+if (pathName == 'Visualization') {
+    visualizationNav.classList.add("bg-gray-900", "text-white", "px-3", "py-2", "rounded-md", "text-sm", "font-medium")
+    homeNav.classList.add("text-gray-300", "hover:bg-gray-700", "hover:text-white", "px-3", "py-2", "rounded-md", "text-sm", "font-medium",)
+    coursesNav.classList.add("text-gray-300", "hover:bg-gray-700", "hover:text-white", "px-3", "py-2", "rounded-md", "text-sm", "font-medium",)
+}
+if (pathName == "") {
+    homeNav.classList.add("bg-gray-900", "text-white", "px-3", "py-2", "rounded-md", "text-sm", "font-medium")
+    visualizationNav.classList.add("text-gray-300", "hover:bg-gray-700", "hover:text-white", "px-3", "py-2", "rounded-md", "text-sm", "font-medium",)
+    coursesNav.classList.add("text-gray-300", "hover:bg-gray-700", "hover:text-white", "px-3", "py-2", "rounded-md", "text-sm", "font-medium",)
+}
+console.log(pathName);
