dd711f238366ee81d0d8f8cc129a87d064130d19
mohammad maso <mohammadmasoudie@gmail.com>
2021-05-28 22:41:38 +0430

    spinner timer 5000

--- Spinner.js
+++ Spinner.js
@@ -152,7 +152,7 @@ setVisible('.page', false);
 setVisible('#whirlpool', true);
 
 document.addEventListener('DOMContentLoaded', () =>
-    wait(   2000).then(() => {
+    wait(   5000).then(() => {
         setVisible('.page', true);
         setVisible('#whirlpool', false);
     }));
